7f77219064775b017e29f0d3bc7d25ace7128261
Downwine <ksenia.ashess@gmail.com>
2022-12-22 12:07:58 +0300

    Пушнула

--- app/src/main/java/com/example/myapplication/ui/cases/CaseFragment.java
+++ app/src/main/java/com/example/myapplication/ui/cases/CaseFragment.java
@@ -10,10 +10,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
-import com.example.myapplication.databinding.FragmentCasesBinding;
 import com.example.myapplication.entities.CrimCase;
-import com.example.myapplication.ui.cases.CaseAdapter;
-import com.example.myapplication.ui.cases.CaseDataModel;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,7 +22,7 @@ import java.util.List;
 
 public class CaseFragment extends Fragment {
     private com.example.myapplication.databinding.FragmentCasesBinding binding;
-    private DatabaseReference dbPrisoner;
+    private DatabaseReference dbCases;
     private CaseAdapter adapter;
     List<CaseDataModel> cases = new ArrayList<CaseDataModel>();
 
@@ -36,7 +33,7 @@ public class CaseFragment extends Fragment {
         View root = binding.getRoot();
 
         FirebaseDatabase pisosData = FirebaseDatabase.getInstance();
-        dbPrisoner  = pisosData.getReference("Prisoner");
+        dbCases = pisosData.getReference("Сrim_Case");
 
 //        Intent myIntent = new Intent(getContext(), Cases_ReadActivity.class);
 //        myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -48,10 +45,12 @@ public class CaseFragment extends Fragment {
         // устанавливаем для списка адаптер
         recyclerView.setAdapter(adapter);
 
+        getCaseFromDB();
+
         return root;
     }
 
-    private void getDataFromDB(){
+    private void getCaseFromDB(){
         ValueEventListener vListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot)
@@ -71,6 +70,6 @@ public class CaseFragment extends Fragment {
 
             }
         };
-        dbPrisoner.addValueEventListener(vListener);
+        dbCases.addValueEventListener(vListener);
     }
 }
